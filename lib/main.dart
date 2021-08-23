cfa0f6e944f7f6981d877aa821bb15387efc47eb
Phong Nguyen <nguyentruongphong113@gmail.com>
2021-08-23 12:20:43 +0700

    Fist time commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Assignment_2'),
+          centerTitle: true,
+        ),
+      ),
+    );
+  }
+}
